87a2abd6003287bc0f10ffa3c21c9ca9700a6204
email4prasanth <email4prasanth@gmail.com>
2025-07-10 16:10:02 +0530

    removed api.aitechlearn.xyz

--- frontend-infra/cloudfront.tf
+++ frontend-infra/cloudfront.tf
@@ -42,7 +42,8 @@ resource "aws_cloudfront_distribution" "frontend" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.cdn_cert.arn
+    # acm_certificate_arn      = aws_acm_certificate.cdn_cert.arn
+    cloudfront_default_certificate = true
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
