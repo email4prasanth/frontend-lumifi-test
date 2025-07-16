3beba03e58585421b54ec875d0c064b46cc26456
email4prasanth <email4prasanth@gmail.com>
2025-07-16 16:20:05 +0530

    testing fe

--- frontend-infra/upload_validation.tf
+++ frontend-infra/upload_validation.tf
@@ -1,5 +1,24 @@
 
-# # use for testing in powershell
+# # # use for testing in powershell
+# # resource "null_resource" "cloudfront_invalidation" {
+# #   triggers = {
+# #     index = filesha256("index.html")
+# #   }
+
+# #   provisioner "local-exec" {
+# #     command     = <<EOT
+# #       aws cloudfront create-invalidation `
+# #         --distribution-id ${aws_cloudfront_distribution.frontend.id} `
+# #         --paths "/*" 
+# #         # --profile lumifitest
+# #     EOT
+# #     interpreter = ["PowerShell", "-Command"]
+# #   }
+
+# #   depends_on = [aws_s3_object.index]
+# # }
+
+# # use for testing when it is yml cicd
 # resource "null_resource" "cloudfront_invalidation" {
 #   triggers = {
 #     index = filesha256("index.html")
@@ -7,31 +26,12 @@
 
 #   provisioner "local-exec" {
 #     command     = <<EOT
-#       aws cloudfront create-invalidation `
-#         --distribution-id ${aws_cloudfront_distribution.frontend.id} `
-#         --paths "/*" 
-#         # --profile lumifitest
+#       aws cloudfront create-invalidation \
+#         --distribution-id ${aws_cloudfront_distribution.frontend.id} \
+#         --paths "/*"
 #     EOT
-#     interpreter = ["PowerShell", "-Command"]
+#     interpreter = ["/bin/bash", "-c"]
 #   }
 
 #   depends_on = [aws_s3_object.index]
 # }
-
-# use for testing when it is yml cicd
-resource "null_resource" "cloudfront_invalidation" {
-  triggers = {
-    index = filesha256("index.html")
-  }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-      aws cloudfront create-invalidation \
-        --distribution-id ${aws_cloudfront_distribution.frontend.id} \
-        --paths "/*"
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-
-  depends_on = [aws_s3_object.index]
-}
